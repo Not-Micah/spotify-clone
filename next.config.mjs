ee7d814fa784befc667d1d1297b8775d47eb9394
Not-Micah <micahtid@gmail.com>
2024-03-15 10:37:09 +0700

    first commit -stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "iapypzovtoelynfxnhlz.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
